dd091fa5f6fc143d3eb4a30421df2a0f3d212cba
Santiago Vasquez <svasquez@pivotal.io>
2020-02-04 07:42:04 -0500

    symlinks support

--- bar.sh
+++ bar.sh
@@ -1,2 +1,17 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "$SCRIPT_DIR"
+get_script_dir () {
+  SOURCE="${BASH_SOURCE[0]}"
+  
+  # while $SOURCE is a symlink, resolve it
+  while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$( readlink "$SOURCE" )"
+    
+    # if $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  done
+
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  echo "$DIR"
+}
+
+echo "$(get_script_dir)"
